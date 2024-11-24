91d03ec1d5c1c47fd95b428907c07185301d7238
Schrocat-Academic <walker.michael.luke@gmail.com>
2024-11-24 11:17:33 +1100

    restarted elmo repository

---
+++ README.md
@@ -0,0 +1,7 @@
+Project Elmo, born of my need to economise the Grover algorithm
+
+This project requires qiskit to be installed, recommended by 
+pip install qiskit  
+
+but best to consult website q-ctrl.com/qiskit/
+
---
+++ elmo.py
@@ -0,0 +1,41 @@
+# elmo.py
+# copyright Michael Walker 2024
+#
+# This circuit create extracts a desired (flagged) component from a given state.
+# It works by creating two independent copies of the state and flipping the
+# flag qubit as done for a Grover operation except:
+# 1) the flip is controlled by a qubit in the |+> state,
+# 2) the flipped components have their sign reversed for one copy.
+# Note that the flag qubit is absorbed into the state.
+# 
+# A Hadamard on the controlling |+> qubit then yields the final state
+# |0>|unflagged>|flagged> + |1>|flagged>|unflagged>
+
+def elmo(qreg1,qreg2,symmetric=0) :
+  qc.h(symmetrize)
+  for qubit in qreg2 :
+    qc.x(qubit)
+  qc.h(symmetrize)
+  qc.mcx(qreg2,symmetrize)
+  qc.h(symmetrize)
+  # Initialise qregs
+  for qubit in qreg1 :
+    qc.h(qubit)
+  for qubit in qreg2 :
+    qc.h(qubit)
+  # Flag sought component |11>
+  qc.h(symmetrize)
+  qc.mcx(qreg2,symmetrize)
+  qc.h(symmetrize)
+  #
+  qc.x(symmetrize)
+  qc.h(symmetrize)
+  qc.mcx(qreg1,symmetrize)
+  qc.h(symmetrize)
+  qc.x(symmetrize)
+  # Take symmetric and antisymmetric components
+  qc.h(symmetrize)
+
+  qc.measure_all()
+  return qc
+
