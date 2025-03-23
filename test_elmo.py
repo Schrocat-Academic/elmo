fe131db79457e07e769b44cc4c604550799a17a2
Schrocat-Academic <walker.michael.luke@gmail.com>
2025-03-23 17:53:56 +1100

    Initiating branch with qft based elmo method. Introduced val and qft qubits.

--- test_elmo.py
+++ test_elmo.py
@@ -1,22 +1,20 @@
 # test_elmo.py
 # Calls various elmo circuits to test them
 #
-# Copyright Michael Walker 2024
+# Copyright Michael Walker 2025
 
 from qiskit.circuit import QuantumCircuit
 from qiskit.primitives import Sampler
 #from qiskit.primitives import StatevectorSampler
 import elmo
 
-def test_elmo(nq_qreg) :
-  symmetrize = 0
-  qreg1 = [qubit for qubit in range(symmetrize+1,symmetrize+1+nq_qreg)]
-  qreg2 = [qubit for qubit in range(qreg1[-1]+1,qreg1[-1]+1+nq_qreg)]
-  creg0 = qreg2[-1] + 1
-  creg1 = [creg0 + qubit for qubit in qreg1]
-  creg2 = [creg0 + qubit for qubit in qreg2]
-  qreg = [symmetrize] + qreg1 + qreg2
-  creg = [creg0] + creg1 + creg2
+def test_elmo(nq_val,nq_qft) :
+  qval = [qubit for qubit in range(nq_val)]
+  qqft = [[qval[-1] + 1 + qindex*nq_qft + qubit for qubit in range(nq_qft)] for qindex in range(nq_val)]
+  cval = [qqft[-1][-1] + 1 + qubit for qubit in qval]
+  cqft = [qqft[-1][-1] + 1 + qubit for qubit in qqft]
+  qreg = qval + qqft
+  creg = cval + cqft
   qc = elmo.elmo(QuantumCircuit(qreg[-1]+1),qreg1,qreg2,0)
   qc.measure_all(qreg,creg)
   print(qc.draw())
